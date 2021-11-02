4dbbd22a724d4d88f5ee084e9fa38339bee042b8
Vinicius Oliveira <viniciuso.contato@gmail.com>
2021-11-02 16:59:05 -0300

    class(python-variables): 02-variable-names

---
+++ python-variables/02-variable-names.py
@@ -0,0 +1,13 @@
+# Variable names must make sense
+
+# Variable names can't start with a number
+
+# Variable names can't contain spaces
+
+# Variable names can't contain symbols
+
+# Python convention is to use lowercase letters
+
+# Python convention is to use underscores
+
+# Python follows the pep8 convention
