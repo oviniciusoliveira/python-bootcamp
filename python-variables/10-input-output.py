3ebfdf4529cb6d7c8f6ddff76047613a1533c5dc
Vinicius Oliveira <viniciuso.contato@gmail.com>
2021-11-02 20:47:59 -0300

    lesson(python-variables): 10-input-output

---
+++ python-variables/10-input-output.py
@@ -0,0 +1,14 @@
+print('*-----Print Output-----*')
+print('Hello Python')
+print(101)
+print([1, 2, 3])
+
+first_name = 'Antonio'
+last_name = 'Belchior'
+
+print('My first name is {} and my last name is {}, so my full name is {} {}'.format(
+    first_name, last_name, first_name, last_name))
+
+print('\n*-----Print Input-----*')
+number = input('Enter a number: ')
+print("You entered the number: ", int(number))
