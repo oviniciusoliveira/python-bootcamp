c525400acda158c4ccd237759d8adc2a74ff57d0
Vinicius Oliveira <viniciuso.contato@gmail.com>
2021-11-02 20:01:11 -0300

    lesson(python-variables): 08-number-methods-operations

--- notes.md
+++ notes.md
@@ -1,8 +1,11 @@
 extensions:
+
 - python
 - code runner
 - AREPL
 
 libraries:
+
 - autopep8: code formatter (pep 8)
 - pylint : linter
+- math : mathematic functions
---
+++ python-variables/08-number-methods-operations.py
@@ -0,0 +1,35 @@
+import math
+
+print('*-----Arithmetic operations-----*')
+print(23 + 12)
+print(50 - 12)
+print(23 * 2)
+print(23 / 12)
+print(23 // 4)
+print(23 % 5)
+print(23 ** 2)
+
+print('*-----Augmented Assignment Operator-----*')
+number = 10
+print(number)
+# number = number + 5
+number += 5
+print(number)
+number *= 2
+print(number)
+number /= 5
+print(number)
+
+print('*-----Round-----*')
+print(round(2.5))
+print(round(213.542353))
+
+print('*-----Absolute-----*')
+print(abs(-23))
+print(abs(+1.34))
+
+print('*-----Math Library-----*')
+print(math.sin(math.pi / 2))
+print(math.cos(0))
+print(math.ceil(2.1))
+print(math.floor(2.9))
