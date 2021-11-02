2f70b1573feeea99c068cad60b6a8af910aff408
Vinicius Oliveira <viniciuso.contato@gmail.com>
2021-11-02 19:48:05 -0300

    lesson(python-variables): 06-escape-sequences

---
+++ python-variables/06-escape-sequences.py
@@ -0,0 +1,10 @@
+# Escape the ' caracter
+story_description = 'It\'s a touching story'
+print(story_description)
+
+# Escape the \ caracter
+story_description = "It\\'s a touching story"
+print(story_description)
+
+# Break Line
+story_description = 'It\'s a \n touching \n story'
