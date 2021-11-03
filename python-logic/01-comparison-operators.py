07fc5241583145e4e5552d87d7718c519e35c9a8
Vinicius Oliveira <viniciuso.contato@gmail.com>
2021-11-02 21:15:20 -0300

    lesson(python-logic): 01-comparison-operators

---
+++ python-logic/01-comparison-operators.py
@@ -0,0 +1,14 @@
+print(14 > 4)
+print(14 >= 4)
+print(14 < 4)
+print(14 <= 4)
+print(14 == 14)
+print(14 == '14')
+print(14 != '14')
+
+# Numerical representation
+print(ord('c'))
+print(ord('C'))
+
+print('cat' > 'dog')
+print('cat' == 'CAT')
