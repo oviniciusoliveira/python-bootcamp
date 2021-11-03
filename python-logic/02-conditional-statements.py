dcddf74a86e7cfa54aea115b4e22c5c843d953e4
Vinicius Oliveira <viniciuso.contato@gmail.com>
2021-11-02 21:24:04 -0300

    lesson(python-logic): 02-conditional-statements

---
+++ python-logic/02-conditional-statements.py
@@ -0,0 +1,13 @@
+temperature = 18
+if temperature > 30:
+    print("It's a hot day")
+else:
+    print("It's not a hot day")
+
+hour = 16
+if hour < 12:
+    print("It's morning")
+elif hour < 18:
+    print("It's afternoon")
+else:
+    print("It's evening")
