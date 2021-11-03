7a2615b50ccc6e174ba7c9696ae092b84fe6a17e
Vinicius Oliveira <viniciuso.contato@gmail.com>
2021-11-03 15:49:56 -0300

    lesson(python-loops): 02-nested-loops

---
+++ python-loops/02-nested-loops.py
@@ -0,0 +1,10 @@
+for x in range(3):
+    for y in range(3):
+        print(f"Point {x}, {y}")
+
+print("\n")
+
+for x in range(1):
+    for y in range(2):
+        for z in range(3):
+            print(f"Point {x}, {y}, {z}")
