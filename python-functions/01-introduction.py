e7201cf57a6796e537729707be5303b525afa56f
Vinicius Oliveira <viniciuso.contato@gmail.com>
2021-11-04 20:35:06 -0300

    lesson(python-functions): 01-introduction

---
+++ python-functions/01-introduction.py
@@ -0,0 +1,29 @@
+def book():
+    print('Thriller')
+
+
+book()
+
+print('\n')
+
+success = True
+
+
+def operation():
+    if success:
+        print('Success')
+    else:
+        print('Failure')
+
+
+operation()
+
+print('\n')
+
+
+def sum():
+    for number in range(4, 34, 4):
+        print(number)
+
+
+sum()
