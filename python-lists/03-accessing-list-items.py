639427d35e931861a03d2bdce6e828092e099766
Vinicius Oliveira <viniciuso.contato@gmail.com>
2021-11-25 01:09:15 -0300

    lesson(python-lists): 03-accessing-list-items

---
+++ python-lists/03-accessing-list-items.py
@@ -0,0 +1,34 @@
+collection = [28, 'hello', 1.0, True, 'world', 'banana', 3, 'cube']
+
+collection[4] = 'Python'
+
+print(collection)
+print(collection[0])
+print(collection[2])
+
+print('\n 0:3')
+print(collection[0:3])
+
+print('\n :2')
+print(collection[:2])
+
+print('\n 2:')
+print(collection[2:])
+
+print('\n -1')
+print(collection[-1])
+
+print('\n :')
+print(collection[:])
+
+print('\n ::2')
+print(collection[::2])
+
+print('\n ::-1')
+print(collection[::-1])
+
+print('\n 2::2')
+print(collection[2::2])
+
+print('\n 2:5:2')
+print(collection[2:5:2])
